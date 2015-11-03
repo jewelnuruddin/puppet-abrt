fe3903b9b13397adb81dc5a317953798f752bcd7
Cristian Falcas <falcas@optymyze.com>
2015-11-03 15:58:38 +0200

    fix spec tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,30 @@
 require 'spec_helper'
-describe 'abrt' do
 
-  context 'with defaults for all parameters' do
-    it { should contain_class('abrt') }
+describe 'abrt' do
+  context 'with defaults for all parameters on RedHat' do
+    let :facts do
+      {
+        :kernel   => 'Linux',
+        :osfamily => 'RedHat',
+      }
+    end
+
+    context "on el6" do
+      let :facts do
+        super().merge({
+          :operatingsystemmajrelease => '6',
+        })
+      end
+      it { should contain_class('abrt') }
+    end
+
+    context "on el7" do
+      let :facts do
+        super().merge({
+          :operatingsystemmajrelease => '6',
+        })
+      end
+      it { should contain_class('abrt') }
+    end
   end
 end
