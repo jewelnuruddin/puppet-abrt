19bbd58f702b6ef413acbfc653598269194ac593
jewelnuruddin <jewel.nuruddin@fout.jp>
2019-04-26 17:56:53 +0900

    add 'mocha'

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,12 +2,12 @@ require 'rubygems'
 require 'puppetlabs_spec_helper/module_spec_helper'
 require 'rspec-puppet-facts'
 #require 'mocha/api'
-#require 'mocha/minitest'
+require 'mocha/minitest'
 #require 'mocha/test_unit'
-#include RspecPuppetFacts
+include RspecPuppetFacts
 
-#RSpec.configure do |config|
-#  config.mock_with :rspec do |c|
-#    c.syntax = :expect
-#  end
-#end
+RSpec.configure do |config|
+  config.mock_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
