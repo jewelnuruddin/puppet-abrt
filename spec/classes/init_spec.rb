fa9d677cc95de86422cc87889a36e557c0c7091a
jewelnuruddin <jewel.nuruddin@fout.jp>
2019-04-26 16:43:56 +0900

    Use ruby 2.3.1 for puppet3

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,14 +1,29 @@
-require 'spec_helper'
+#require 'spec_helper'
 
+#describe 'abrt' do
+#  on_supported_os.each do |os, facts|
+#    context "with defaults on #{os}" do
+#      let(:facts) do
+#        facts.merge({:puppetmaster => 'localhost.localdomain'})
+#      end
+#      let(:params) { { :package_ensure => 'installed' } }
+#      it { should compile.with_all_deps }
+#      it { should contain_class('abrt') }
+#    end
+#  end
+#end
+
+require 'spec_helper'
 describe 'abrt' do
-  on_supported_os.each do |os, facts|
-    context "with defaults on #{os}" do
-      let(:facts) do
-        facts.merge({:puppetmaster => 'localhost.localdomain'})
-      end
-      let(:params) { { :package_ensure => 'installed' } }
-      it { should compile.with_all_deps }
-      it { should contain_class('abrt') }
-    end
+
+  context 'with defaults for all parameters' do
+    it { should contain_package('abrt').with_ensure('present')}
+    it { should contain_service('abrtd').with_ensure('running') }
   end
+
+  context 'with active => "false"' do
+    let(:params) { {:active => false } }
+    it { should contain_service('abrtd').with_ensure('stopped') }
+  end
+
 end
