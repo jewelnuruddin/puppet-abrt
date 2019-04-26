9a483c24880e9c45d19c85a880450522461a0f71
jewelnuruddin <jewel.nuruddin@fout.jp>
2019-04-26 17:42:35 +0900

    add 'mocha/test_unit'

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 
 require 'puppetlabs_spec_helper/module_spec_helper'
 require 'rspec-puppet-facts'
+require 'mocha/test_unit'
 include RspecPuppetFacts
 
 RSpec.configure do |config|
