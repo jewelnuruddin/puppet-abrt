96e24798a234e79fc9fa1da6435a02a58d9a08f2
jewelnuruddin <jewel.nuruddin@fout.jp>
2019-04-27 11:50:51 +0900

    basic build check'

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,28 +1,26 @@
 require 'spec_helper'
 
-describe 'abrt' do
-  let(:title) { 'abrt' }
-  let(:node) { 'localhost' }
-#  it { is_expected.to compile }
-#  it { is_expected.to compile.with_all_deps }
-  context 'RedHat' do
-    let(:facts) { {
-      :osfamily => 'RedHat',
-      :operatingsystem => 'CentOS',
-      :architecture => 'x86_64'
-    } }
-   end
-end
-
 #describe 'abrt' do
-#  on_supported_os.each do |os, facts|
-#    context "with defaults on #{os}" do
-#      let(:facts) do
-#        facts.merge({:puppetmaster => 'localhost.localdomain'})
-#      end
-#      let(:params) { { :package_ensure => 'installed' } }
-#      it { should compile.with_all_deps }
-#      it { should contain_class('abrt') }
-#    end
-#  end
+#  let(:title) { 'abrt' }
+#  let(:node) { 'localhost' }
+#  context 'RedHat' do
+#    let(:facts) { {
+#      :osfamily => 'RedHat',
+#      :operatingsystem => 'CentOS',
+#      :architecture => 'x86_64'
+#    } }
+#   end
 #end
+
+describe 'abrt' do
+  on_supported_os.each do |os, facts|
+    context "with defaults on #{os}" do
+      let(:facts) do
+        facts.merge({:puppetmaster => 'localhost.localdomain'})
+      end
+      let(:params) { { :package_ensure => 'installed' } }
+      it { should compile.with_all_deps }
+      it { should contain_class('abrt') }
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 require 'rubygems'
 require 'puppetlabs_spec_helper/module_spec_helper'
-#require 'rspec-puppet-facts'
+require 'rspec-puppet-facts'
 #require 'mocha/api'
 #require 'mocha/minitest'
 #require 'mocha/test_unit'
