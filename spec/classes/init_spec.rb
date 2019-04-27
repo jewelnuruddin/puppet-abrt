ce0a14a9ff163d8f9ccc2835d0957f6d787fc1ee
jewelnuruddin <jewel.nuruddin@fout.jp>
2019-04-27 11:25:00 +0900

    basic build check'

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,14 +1,23 @@
 require 'spec_helper'
 
 describe 'abrt' do
-  on_supported_os.each do |os, facts|
-    context "with defaults on #{os}" do
-      let(:facts) do
-        facts.merge({:puppetmaster => 'localhost.localdomain'})
-      end
-      let(:params) { { :package_ensure => 'installed' } }
-      it { should compile.with_all_deps }
-      it { should contain_class('abrt') }
-    end
-  end
+  let(:title) { 'abrt' }
+  let(:node) { 'localhost' }
+  it { is_expected.to compile }
+  it { is_expected.to compile.with_all_deps }
 end
+
+
+
+#describe 'abrt' do
+#  on_supported_os.each do |os, facts|
+#    context "with defaults on #{os}" do
+#      let(:facts) do
+#        facts.merge({:puppetmaster => 'localhost.localdomain'})
+#      end
+#      let(:params) { { :package_ensure => 'installed' } }
+#      it { should compile.with_all_deps }
+#      it { should contain_class('abrt') }
+#    end
+#  end
+#end
